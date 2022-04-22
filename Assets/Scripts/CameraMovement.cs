bcbe9adf66bfd6394f8d18783d5d01df14e16875
Juan Manuel <68016784+madpingui@users.noreply.github.com>
2022-04-21 22:24:31 -0500

    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public float cameraSpeed = 0.04f;
+
+	private void FixedUpdate () {
+		transform.position = new Vector3 (transform.position.x, transform.position.y + cameraSpeed, transform.position.z);
+	}
+
+}
